bef2d0130fb7bc6fbe245f984eaab735b04ce5b4
AIGLEKIVOL <112876945+AIGLEKIVOL@users.noreply.github.com>
2022-11-14 15:40:14 +0100

    correction orthographe

--- src/polluxPak/Etat.java
+++ src/polluxPak/Etat.java
@@ -3,15 +3,15 @@ package polluxPak;
 public class Etat {
 	private String nom;
 	private int num;
-	public static Etat P0=new Etat ("Etat Initiale",1);
+	public static Etat P0=new Etat ("Etat Initial",1);
 	public static Etat B1=new Etat ("Premier But Mis",2);
 	public static Etat BUT=new Etat ("But Mis",3);
 	public static Etat PALET_A=new Etat ("Scan reussi",4);
 	public static Etat PALET0=new Etat ("Echec scan",5);
 	public static Etat VERIFICATI0N=new Etat("verification",6);
-	public static Etat SECOUR1=new Etat ("Methode de Secour1",7);
-	public static Etat FINPARCOUR=new Etat ("Fin Secour1",8);
-	public static Etat SECOUR2=new Etat ("Methode de Secour2",9);
+	public static Etat SECOURS1=new Etat ("Methode de Secours1",7);
+	public static Etat FINPARCOURS=new Etat ("Fin Secours1",8);
+	public static Etat SECOURS2=new Etat ("Methode de Secours2",9);
 
 
 	private Etat() {
